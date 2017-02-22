b3fbb36464596b2be2ae1b19748abce0dea1a855
hassy <hassy@inl.ics.keio.ac.jp>
2017-02-22 15:41:37 +0900

    step3comlete

--- step3.c
+++ step3.c
@@ -7,8 +7,6 @@
 
 
 void split_cmd(char* cmd, int* ac, char* av[]);
-void print_arg(char* arg);
-void print_args(int ac, char** av);
 void split_proc(int* ac, char* av[], int *Pac, char* Pav[], int* count);
 int countPipe(int ac, char* av[]);
 
@@ -21,8 +19,9 @@ int main(void)
 	char *Pav[MAXWORDNUM];
 	int Pac;
 
-	int i = 0;
-	int count = 0;
+	int i;
+	int count;
+	int pronum;
 
 	while (1) {
 		char cmd[MAXCMDLEN] = {0};
@@ -33,17 +32,14 @@ int main(void)
 		}
 
 		split_cmd(cmd, &ac, av);
-		printf("Total Process = %i\n", countPipe(ac, av) + 1);
-		while (count < (countPipe(ac, av) + 1)) {
+		pronum = countPipe(ac, av) + 1;
+		for (count = 0; count < pronum; count++) {
 			split_proc(&ac, av, &Pac, Pav, &count);
-			printf("Proc %i\nPac = %i\n", count + 1, Pac);
-			while (i < Pac) {
-				printf("Pav[%i] = %s\n", i, av[i]);
-				i++;
+			printf("Proc %i\nPac = %i\n", count, Pac);
+			for (i = 0; i < Pac; i++) {
+				printf("Pav[%i] = %s\n", i, Pav[i]);
 			}
 			printf("\n");
-			i = 0;
-			count++;
 		}
 	}
 }
@@ -66,19 +62,12 @@ void split_cmd(char* cmd, int* ac, char* av[]) {
 	
 }
 
-void print_arg(char* arg);
-void print_args(int ac, char** av) {
-	int i = 0;
-	while (i < ac) {
-		printf("av[%i]: %s\n", i, av[i]);
-		i++;
-	}
-}
-
 void split_proc(int* ac, char* av[], int *Pac, char* Pav[], int* count) {
 	int i = 0;
 	int j = 0;
-	int k = 0;//Pavに格納
+	int k;
+	*Pac = 0;
+
 	while (i < *count) {
 		while (*av[j] != '|') {
 			j++;
@@ -86,12 +75,13 @@ void split_proc(int* ac, char* av[], int *Pac, char* Pav[], int* count) {
 		j++;
 		i++;
 	}
-	while (*av[j] != '|') { 
-		*Pav[k] = *av[j];
-		k++;
-		j++;
+
+	for (k = 0; j < *ac; j++, k++) {
+		if (*av[j] != '|') { 
+			Pav[k] = av[j];
+			*Pac = *Pac + 1;
+		} else break;
 	}
-	*Pac = k;
 }
 
 int countPipe(int ac, char* av[]) {
